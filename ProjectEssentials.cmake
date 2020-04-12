a91a65f229a83fe97c3d0a7f1dfc90aa475afded
Adam Attila Farkas <wolfee@Adams-MacBook-Pro.local>
2020-04-13 01:06:53 +0200

    bugfix

--- ProjectEssentials.cmake
+++ ProjectEssentials.cmake
@@ -1,6 +1,8 @@
-include(CompilerWarnings.cmake)
-include(StandardProjectSettings.cmake)
-include(StaticAnalyzers.cmake)
-
-add_library(project_warnings INTERFACE)
-set_project_warnings(project_warnings)
+function(setup_project_essentials foldername)
+  include(${foldername}/CompilerWarnings.cmake)
+  include(${foldername}/StandardProjectSettings.cmake)
+  include(${foldername}/StaticAnalyzers.cmake)
+  
+  add_library(project_warnings INTERFACE)
+  set_project_warnings(project_warnings)
+endfunction()
