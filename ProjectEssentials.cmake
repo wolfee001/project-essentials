ae1e75b8c890da440ec32bdd0360c1abd2e938b5
Adam Attila Farkas <wolfee@Adams-MacBook-Pro.local>
2020-04-13 01:51:41 +0200

    bugfix

--- ProjectEssentials.cmake
+++ ProjectEssentials.cmake
@@ -1,10 +1,12 @@
-function(setup_project_essentials foldername)
-  include(${foldername}/CompilerWarnings.cmake)
-  include(${foldername}/StandardProjectSettings.cmake)
-  include(${foldername}/StaticAnalyzers.cmake)
+if(NOT PROJECT_ESSENTIALS_DIR)
+  message(SEND_ERROR "PROJECT_ESSENTIALS_DIR not set!")
+endif()
 
-  file(COPY ${foldername}/.clang-format ${foldername}/.clang-tidy DESTINATION ${PROJECT_SOURCE_DIR})
+include(${PROJECT_ESSENTIALS_DIR}/CompilerWarnings.cmake)
+include(${PROJECT_ESSENTIALS_DIR}/StandardProjectSettings.cmake)
+include(${PROJECT_ESSENTIALS_DIR}/StaticAnalyzers.cmake)
 
-  add_library(${PROJECT_NAME}_warnings INTERFACE)
-  set_project_warnings(${PROJECT_NAME}_warnings)
-endfunction()
+file(COPY ${PROJECT_ESSENTIALS_DIR}/.clang-format ${PROJECT_ESSENTIALS_DIR}/.clang-tidy DESTINATION ${PROJECT_SOURCE_DIR})
+
+add_library(${PROJECT_NAME}_warnings INTERFACE)
+set_project_warnings(${PROJECT_NAME}_warnings)
